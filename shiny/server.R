7c842f6bb6f1cb06ff50d72c0fb474169dbb02d4
Monica Chang <mychang07@gmail.com>
2020-10-16 18:19:29 -0400

    updated about and data tabs of shiny app

--- shiny/server.R
+++ shiny/server.R
@@ -1,16 +1,28 @@
-
+ada_request_2018_2020 <- readRDS(file = "data/ada_request_2018_2020.rds") 
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
+    output$request_types_plot <- renderPlot({
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        request_types <- ada_request_2018_2020 %>%
+            pivot_longer(cols = req_a_anim:req_a_oth, names_to = "accomodation_type", values_to = "requests") %>%
+            group_by(accomodation_type) %>%
+            summarize(total_requests = sum(requests), .groups = "drop") %>%
+            arrange(desc(total_requests)) %>%
+            mutate(pct_requests = total_requests/sum(total_requests))
+        
+        # Create a plot showing the requests by accommodation type.
+        
+        request_types %>%
+            ggplot(aes(x = pct_requests, y = fct_reorder(accomodation_type, pct_requests))) +
+            geom_col() +
+            theme_linedraw() +
+            scale_y_discrete(labels = c("Caretaker", "Wheelchair", "Cooking facilities", "Physical modification", "Change in regulation", "Non-carpeted", "First floor/elevator", "Assistance animal", "Other", "Scattered site", "Service providers")) +
+            labs(title = "Percentage of ADA requests by accomodation type (2015-2019)",
+                 x = "Percentage of requests", 
+                 y = "Accomodation type",
+                 caption = "Source: Department of Housing & Community Development")
 
     })
 
--- shiny/ui.R
+++ shiny/ui.R
@@ -15,10 +15,23 @@ library(tidyverse)
 shinyUI(
     navbarPage(theme = shinytheme("flatly"), 
         "Garcia vs DHCD",
+               tabPanel("About", 
+                   p("For my final project, I am working with the Greater Boston
+                   Legal Services Housing Unit to provide data analytics support
+                   on their class action lawsuit",
+                     em("(Garcia vs. DHCD)"), 
+                     "against the MA Emergency Assistance Shelter system.",
+                     "I am analyzing data specifically related to the lawsuit claim under the Americans With Disabilities Act.",
+                     "Many families that request a disability-related accommodation are told that their request will be met when administratively feasible.",
+                     "However, this can take months.", 
+                     "I am cleaning and merging data provided by the MA Department of Housing and Community Development to analyze how long these delays take, and whether certain types of accommodation requests result in longer delays.",
+                     "I am merging data for accommodation requests, shelter tranfers, shelter characteristics, and exit information to answer these questions.",
+                     "I am also merging data across time since GBLS received one batch of data in 2018 and another batch in 2020."),
+                   p("So far, I have cleaned and merged the ADA request data from 2018 and 2020. The bar graph under the Data tab shows the percentages of ADA requests by accommodation type."),
+                   p(a("Here is a link to my Github repo!", href = "https://github.com/monica-chang/Garcia-vs-DHCD"))),
+               
                tabPanel(
-                   "Disability-related claim"),
-               # You would add your content within the parentheses above.
-               tabPanel(
-                   "Immediate placement claim"
+                   "Data",
+                   plotOutput("request_types_plot")
                )
     ))
