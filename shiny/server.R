2c0f829d8c683b61a5b198b4e7d103df5231ff70
Monica Chang <mychang07@gmail.com>
2020-12-02 20:45:03 -0500

    added html file

--- shiny/server.R
+++ shiny/server.R
@@ -215,86 +215,76 @@ shinyServer(function(input, output) {
         
         })
     
-    output$stan_model_tbl <- renderDataTable({ 
-       
-         reactive({
-             
-            # I use stan_glm to generate my linear regression model.
-             
-            stan_glm(reformulate(input$predictor_select, input$outcome_select),
-                     data = supplemented_interesting_ada_transfers_t,
-                     refresh = 0,
-                     seed = 9) %>%
-                 
+    output$stan_model_tbl <- renderTable({ 
+        
+        stan_glm(paste(input$outcome_select, " ~ ", input$predictor_select),
+                 data = supplemented_interesting_ada_transfers_t,
+                 refresh = 0,
+                 seed = 9) %>%
+            
             # I create a tibble to more easily extract the median values from 
             # stan_glm.
-                 
+            
             as_tibble() %>%
             rename(mu = `(Intercept)`,
-                   predictor = input$predictor_select) %>%
+                   predictor = req_scattered_site_placement_unit_co_housing_unit) %>%
             mutate(mu_median = median(mu)) %>%
             mutate(predictor_median = median(predictor)) %>%
             select(mu_median, predictor_median) %>%
             slice(1)
-            
-         })
-        
+      
     })
         
     output$stan_model_plot <- renderPlot({ 
+            
+        # I use stan_glm to generate my linear regression model.
         
-        reactive({
+        stan_glm(paste(input$outcome_select, " ~ ", input$predictor_select),
+                 data = supplemented_interesting_ada_transfers_t,
+                 refresh = 0,
+                 seed = 9) %>%
+        as_tibble() %>%
+        rename(mu = `(Intercept)`,
+               predictor = input$predictor_select) %>%
+        select(mu, predictor) %>%
             
-            # I use stan_glm to generate my linear regression model.
+        # I add mu to the predictor column so that the posterior now
+        # represents the average wait time for individuals without the 
+        # specified predictor instead of the average change in wait time.
             
-            stan_glm(reformulate(input$predictor_select, input$outcome_select),
-                     data = supplemented_interesting_ada_transfers_t,
-                     refresh = 0,
-                     seed = 9) %>%
-            as_tibble() %>%
-            rename(mu = `(Intercept)`,
-                   predictor = input$predictor_select) %>%
-            select(mu, predictor) %>%
-                
-            # I add mu to the predictor column so that the posterior now
-            # represents the average wait time for individuals without the 
-            # specified predictor instead of the average change in wait time.
-                
-            mutate(predictor = predictor + mu) %>%
-            pivot_longer(cols = mu:predictor,
-                         names_to = "parameter",
-                         values_to = "wait_time") %>%
-            ggplot(aes(x = wait_time, color = parameter)) +
-                
-                # I use overlapping histograms to display my two posterior 
-                # distributions.
-                
-                geom_histogram(aes(y = after_stat(count/sum(count))),
-                               alpha = 0.5, 
-                               bins = 100, 
-                               position = "identity") +
-                
-                # I create a more descriptive legend title and labels.
-                
-                scale_fill_discrete(name = "Group", 
-                                    labels = c("Without Predictor", 
-                                               "With Predictor")) +
-                
-                # I add two dotted lines to more clearly show the median value for each 
-                # posterior distribution.
-                
-                geom_vline(xintercept = fit_obj_stats$mu_median, 
-                           color = "red", 
-                           lty = "dashed") +
-                geom_vline(xintercept = fit_obj_stats$predictor_median + fit_obj_stats$mu_median, 
-                           color = "blue", 
-                           lty = "dashed") +
-                labs(title = "Posterior probability distributions",
-                     subtitle = "Predictive distributions of average wait time for individuals with and without the specified predictor",
-                     x = "Average number of days until accommodation met",
-                     y = "Probability")
+        mutate(predictor = predictor + mu) %>%
+        pivot_longer(cols = mu:predictor,
+                     names_to = "parameter",
+                     values_to = "wait_time") %>%
+        ggplot(aes(x = wait_time, color = parameter)) +
             
-        })
+            # I use overlapping histograms to display my two posterior 
+            # distributions.
+            
+            geom_histogram(aes(y = after_stat(count/sum(count))),
+                           alpha = 0.5, 
+                           bins = 100, 
+                           position = "identity") +
+            
+            # I create a more descriptive legend title and labels.
+            
+            scale_fill_discrete(name = "Group", 
+                                labels = c("Without Predictor", 
+                                           "With Predictor")) +
+            
+            # I add two dotted lines to more clearly show the median value for each 
+            # posterior distribution.
+            
+            geom_vline(xintercept = fit_obj_stats$mu_median, 
+                       color = "red", 
+                       lty = "dashed") +
+            geom_vline(xintercept = fit_obj_stats$predictor_median + fit_obj_stats$mu_median, 
+                       color = "blue", 
+                       lty = "dashed") +
+            labs(title = "Posterior probability distributions",
+                 subtitle = "Predictive distributions of average wait time for individuals with and without the specified predictor",
+                 x = "Average number of days until accommodation met",
+                 y = "Probability")
         
 
     })
--- shiny/ui.R
+++ shiny/ui.R
@@ -16,197 +16,190 @@ library(DT)
 
 shinyUI(
     navbarPage(theme = shinytheme("flatly"), 
-        "Garcia vs DHCD",
+        "Garcia vs. DHCD",
         
-             # I create a tab to give an overview of the lawsuit and the
-             # motivation for my final project.
+        # I create a tab to give an overview of the lawsuit and the
+        # motivation for my final project. I also display the plot of 
+        # what percentage of approved ADA requests are met over time. 
+        # The user is able to select the data assumptions they wish to make.
         
-             tabPanel(
-               "The Problem",
-               p("For my final project, I am working with the Greater Boston
-                 Legal Services Housing Unit to provide data analytics support
-                 on their class action lawsuit")),
-        
-        # TODO: Elaborate on this.
-                
-            # I create a tab to give an overview of the ADA requests, 
-            # explain the various accommodations that can be requested, and
-            # provide summary graphs/tables.
-        
-             tabPanel(
-                 "ADA Request Overview",
-                 
-                 # I using subtabs at the top.
-                 
-                 tabsetPanel(
-                   
-                   # Tab one: Approved requests
-                   
-                   tabPanel(
-                     "Approved requests",
-                     h2("Approved requests"),
-                     p("Here is an overview of the types of accommodations that were most frequently approved from 08/04/15 - 12/25/19."),
-                     fluidRow(style = 'padding:30px;',
-                              column(1),
-                              column(9,
-                                     
-                                     # Plot dodged bar graph of all requests and approved requests.
-                                     
-                                     plotOutput("all_requests_plot")),
-                              column(1)),
-                     fluidRow(style = 'padding:30px;',
-                              column(2),
-                              column(8,
-                                     
-                                 # Plot table of all requests and approved requests.
-                                 
-                                 dataTableOutput("all_requests_tbl")),
-                              column(2))
-                     ),
-                   
-                   # TODO: Elaborate on accommodation types.
-                   
-                   # Tab two: Accommodation types
-                   
-                   tabPanel(
-                     "Accommodation types",
-                     h2("Accommodation types"),
-                     p("Here is an overview of the types of accommodations that were most frequently requested in approved ADA requests from 08/04/15 - 12/25/19."),
-                     fluidRow(style = 'padding:30px;',
-                              column(6,
-                                     
-                                 # Plot bar graph of accommodation type breakdown.
-                                 
-                                 plotOutput("request_types_plot")),
-                              column(6,
-                                     
-                                 # Plot table of accommodation type breakdown. 
-                                 
-                                 dataTableOutput("request_types_tbl"))
-                     )  
-                   ),
-                   
-                   # Tab three: Reason types
-                   
-                   tabPanel(
-                     "Reason types",
-                     h2("Reason types"),
-                     p("Here is an overview of the reasons that were most frequently cited for approved ADA requests from 08/04/15 - 12/25/19."),
-                     fluidRow(style = 'padding:30px;',
-                              column(7,
-                                     
-                                 # Plot bar graph of reason type breakdown.
-                                 
-                                 plotOutput("reason_types_plot")),
-                              column(5,
-                                
-                                # Plot table of reason type breakdown. 
-                                     
-                                dataTableOutput("reason_types_tbl"))
-                     ) 
-                   )
-                 )
-             ),
+        tabPanel(
+          "Delays in Accommodation",
+          titlePanel("Delays in Accommodation"),
+          p("Here is an interactive time plot that shows the percentage of approved ADA requests that are met over time:"),
+          sidebarLayout(
             
-            # I create a tab to allow the GBLS team to search up any
-            # individual by PEI and find their date of application, 
-            # date of decision, and date the accommodation was met.
+            sidebarPanel(
+              
+              checkboxInput("interesting_select",
+                            "Only show unit type accommodations",
+                            TRUE),
+              checkboxInput("substitute_select",
+                            "Substitute missing values with transfer data",
+                            TRUE),
+              checkboxInput("hotel_select",
+                            "Assume hotel transfers meet unit type accommodations",
+                            TRUE),
+              dateRangeInput("date_range", "Date range:",
+                             start  = "2015-08-03",
+                             end    = "2019-12-26",
+                             min    = "2015-08-03",
+                             max    = "2019-12-26",
+                             format = "mm/dd/yy",
+                             separator = " - ")
+            ),
+            mainPanel(
+              plotOutput("request_time_plot")
+            )
+            # TODO: Define the assumptions and what they mean/why they matter.
+          )),
         
-            tabPanel(
-              "ADA Request Database",
-              titlePanel("ADA Request Database"),
-              p("Here is a database that shows the date of application, date of decision, and date the accommodation was met for each approved ADA request:"),
-              dataTableOutput("approved_adas")),
+    
+          # I create a tab to give an overview of the ADA requests, 
+          # explain the various accommodations that can be requested, and
+          # provide summary graphs/tables.
         
-            # I create a tab to display the plot of what percetage of 
-            # approved ADA requests are met over time. The user is able to 
-            # select the data assumptions they wish to make.
-      
-            tabPanel(
-              "Delays in DHCD Accommodation",
-              titlePanel("Delays in DHCD Accommodation"),
-              p("Here is an interactive time plot that shows the percentage of approved ADA requests that are met over time:"),
-                 sidebarLayout(
-                   
-                   sidebarPanel(
-                     
-                     checkboxInput("interesting_select",
-                                   "Only show unit type accommodations",
-                                   TRUE),
-                     checkboxInput("substitute_select",
-                                   "Substitute missing values with transfer data",
-                                   TRUE),
-                     checkboxInput("hotel_select",
-                                   "Assume hotel transfers meet unit type accommodations",
-                                   TRUE),
-                     dateRangeInput("date_range", "Date range:",
-                                    start  = "2015-08-03",
-                                    end    = "2019-12-26",
-                                    min    = "2015-08-03",
-                                    max    = "2019-12-26",
-                                    format = "mm/dd/yy",
-                                    separator = " - ")
-                              ),
-                   mainPanel(
-                     plotOutput("request_time_plot")
-                   )
-                 # TODO: Define the assumptions and what they mean/why they matter.
-              )),
+          tabPanel(
+               "ADA Request Overview",
+               
+               # I using subtabs at the top.
+               
+               tabsetPanel(
+                 
+                 # Tab one: Approved requests
+                 
+                 tabPanel(
+                   "Approved requests",
+                   h2("Approved requests"),
+                   p("Here is an overview of the types of accommodations that were most frequently approved from 08/04/15 - 12/25/19."),
+                   fluidRow(style = 'padding:30px;',
+                            column(1),
+                            column(9,
+                                   
+                                   # Plot dodged bar graph of all requests and approved requests.
+                                   
+                                   plotOutput("all_requests_plot")),
+                            column(1)),
+                   fluidRow(style = 'padding:30px;',
+                            column(2),
+                            column(8,
+                                   
+                               # Plot table of all requests and approved requests.
+                               
+                               dataTableOutput("all_requests_tbl")),
+                            column(2))
+                   ),
+                 
+                 # TODO: Elaborate on accommodation types.
+                 
+                 # Tab two: Accommodation types
+                 
+                 tabPanel(
+                   "Accommodation types",
+                   h2("Accommodation types"),
+                   p("Here is an overview of the types of accommodations that were most frequently requested in approved ADA requests from 08/04/15 - 12/25/19."),
+                   fluidRow(style = 'padding:30px;',
+                            column(6,
+                                   
+                               # Plot bar graph of accommodation type breakdown.
+                               
+                               plotOutput("request_types_plot")),
+                            column(6,
+                                   
+                               # Plot table of accommodation type breakdown. 
+                               
+                               dataTableOutput("request_types_tbl"))
+                   )  
+                 ),
+                 
+                 # Tab three: Reason types
+                 
+                 tabPanel(
+                   "Reason types",
+                   h2("Reason types"),
+                   p("Here is an overview of the reasons that were most frequently cited for approved ADA requests from 08/04/15 - 12/25/19."),
+                   fluidRow(style = 'padding:30px;',
+                            column(7,
+                                   
+                               # Plot bar graph of reason type breakdown.
+                               
+                               plotOutput("reason_types_plot")),
+                            column(5,
+                              
+                              # Plot table of reason type breakdown. 
+                                   
+                              dataTableOutput("reason_types_tbl"))
+                   ) 
+                 )
+               )
+           ),
           
-           # I create a tab to display the plot of what percetage of 
-           # approved ADA requests are met over time. The user is able to 
-           # select the data assumptions they wish to make.
-        
-           tabPanel(
-                "Model",
-                sidebarLayout(
-                  sidebarPanel(
-                    selectInput("predictor_select",
-                                "Choose a predictor:",
-                                choices = c("Scattered Site Placement Unit / Co-housing Unit" = "req_scattered_site_placement_unit_co_housing_unit",
-                                            "First Floor or Elevator Access" = "req_first_floor_or_elevator_access",
-                                            "Wheelchair Accessible Placement Unit" = "req_wheelchair_accessible_placement_unit",
-                                            "Mental Health" = "reason_mental_health",
-                                            "Emotional Health" = "reason_emotional_health",
-                                            "Physical Health" = "reason_physical_health",
-                                            "Developmental Disability/Behavioral Health" = "reason_developmental_disability_behavioral"),
-                                selected = "Scattered Site Placement Unit / Co-housing Unit"),
-                    selectInput("outcome_select",
-                                "Choose an outcome:",
-                                choices = c("Days Until Accommodation Met" = "days_until_accommodation_met"),
-                                selected = "Days Until Accommodation Met")
-                  ),
-                  mainPanel(
-                    plotOutput("stan_model_plot")
-                  )),
-                fluidRow(
-                  dataTableOutput("stan_model_tbl")
-                )
-           
-          ),
-        
-        # TODO: Update About page
-        
-         tabPanel("About", 
-               p("For my final project, I am working with the Greater Boston
-                 Legal Services Housing Unit to provide data analytics support
-                 on their class action lawsuit", em("(Garcia vs. DHCD)"), 
-                 "against the MA Emergency Assistance Shelter system.",
-                 "I am analyzing data specifically related to the lawsuit claim
-                 under the Americans With Disabilities Act.",
-                 "Many families that request a disability-related accommodation 
-                 are told that their request will be met when administratively 
-                 feasible.",
-                 "However, this can take months.", 
-                 "I am cleaning and merging data provided by the MA Department 
-                 of Housing and Community Development to analyze how long these 
-                 delays take, and whether certain types of accommodation 
-                 requests result in longer delays.",
-                 "I am merging data for accommodation requests, shelter tranfers, 
-                 shelter characteristics, and exit information to answer these 
-                 questions.",
-                 "I am also merging data across time since GBLS received one 
-                 batch of data in 2018 and another batch in 2020."),
-               p(a("Here is a link to my Github repo!", href = "https://github.com/monica-chang/Garcia-vs-DHCD")))
+          # I create a tab to allow the GBLS team to search up any
+          # individual by PEI and find their date of application, 
+          # date of decision, and date the accommodation was met.
       
-    ))
+          tabPanel(
+            "ADA Request Database",
+            titlePanel("ADA Request Database"),
+            p("Here is a database that shows the date of application, date of decision, and date the accommodation was met for each approved ADA request:"),
+            dataTableOutput("approved_adas")),
+      
+        
+         # I create a tab to display the plot of what percetage of 
+         # approved ADA requests are met over time. The user is able to 
+         # select the data assumptions they wish to make.
+      
+         tabPanel(
+              "Model",
+              sidebarLayout(
+                sidebarPanel(
+                  selectInput("predictor_select",
+                              "Choose a predictor:",
+                              choices = c("Scattered Site Placement Unit / Co-housing Unit" = "req_scattered_site_placement_unit_co_housing_unit",
+                                          "First Floor or Elevator Access" = "req_first_floor_or_elevator_access",
+                                          "Wheelchair Accessible Placement Unit" = "req_wheelchair_accessible_placement_unit",
+                                          "Mental Health" = "reason_mental_health",
+                                          "Emotional Health" = "reason_emotional_health",
+                                          "Physical Health" = "reason_physical_health",
+                                          "Developmental Disability/Behavioral Health" = "reason_developmental_disability_behavioral"),
+                              selected = "Scattered Site Placement Unit / Co-housing Unit"),
+                  selectInput("outcome_select",
+                              "Choose an outcome:",
+                              choices = c("Days Until Accommodation Met" = "days_until_accommodation_met"),
+                              selected = "Days Until Accommodation Met")
+                ),
+                mainPanel(
+                  plotOutput("stan_model_plot")
+                )),
+              fluidRow(
+                dataTableOutput("stan_model_tbl")
+              )
+              # TODO: There must a statistical model, along with an associated discussion of its creation and interpretation
+         
+        ),
+      
+        # TODO: Update About page
+      
+        tabPanel("About", 
+             p("For my final project, I am working with the Greater Boston
+               Legal Services Housing Unit to provide data analytics support
+               on their class action lawsuit", em("(Garcia vs. DHCD)"), 
+               "against the MA Emergency Assistance Shelter system.",
+               "I am analyzing data specifically related to the lawsuit claim
+               under the Americans With Disabilities Act.",
+               "Many families that request a disability-related accommodation 
+               are told that their request will be met when administratively 
+               feasible.",
+               "However, this can take months.", 
+               "I am cleaning and merging data provided by the MA Department 
+               of Housing and Community Development to analyze how long these 
+               delays take, and whether certain types of accommodation 
+               requests result in longer delays.",
+               "I am merging data for accommodation requests, shelter tranfers, 
+               shelter characteristics, and exit information to answer these 
+               questions.",
+               "I am also merging data across time since GBLS received one 
+               batch of data in 2018 and another batch in 2020."),
+             p(a("Here is a link to my Github repo!", href = "https://github.com/monica-chang/Garcia-vs-DHCD")))
+    
+  ))
